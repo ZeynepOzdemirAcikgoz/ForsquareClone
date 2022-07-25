063a4782318a1a282e9d5152c52d80e9bb8d3843
Zeynep Özdemir Açıkgöz <zeynmirgmail.com>
2022-07-25 15:46:09 +0300

    Initial Commit

---
+++ ForsquareClone/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  ForsquareClone
+//
+//  Created by Zeynep Özdemir Açıkgöz on 25.07.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
